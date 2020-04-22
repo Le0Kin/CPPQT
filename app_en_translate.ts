5e21c6b33607e0b097ea06709bed1d43896d5ad1
Le0Kin <44679293+Le0Kin@users.noreply.github.com>
2020-04-22 10:28:35 +0700

    Add files via upload

---
+++ app_en_translate.ts
@@ -0,0 +1,282 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.1" language="en_US">
+<context>
+    <name>MainWindow</name>
+    <message>
+        <location filename="mainwindow.ui" line="20"/>
+        <source>QT Домашнее задание №7</source>
+        <translation>QT HomeWork №7</translation>
+    </message>
+    <message>
+        <location filename="mainwindow.ui" line="38"/>
+        <source>Текстовый редактор</source>
+        <translation>Text editor</translation>
+    </message>
+    <message>
+        <location filename="mainwindow.ui" line="45"/>
+        <source>Имя автора при сохранении в .bin формат</source>
+        <translation>Author name to save to bin format file</translation>
+    </message>
+    <message>
+        <location filename="mainwindow.ui" line="59"/>
+        <location filename="mainwindow.cpp" line="23"/>
+        <source>Добавить таблицу</source>
+        <translation>Add table</translation>
+    </message>
+    <message>
+        <location filename="mainwindow.ui" line="72"/>
+        <source>Отменить изменение</source>
+        <translation>Undo</translation>
+    </message>
+    <message>
+        <location filename="mainwindow.ui" line="91"/>
+        <source>Файл</source>
+        <translation>File</translation>
+    </message>
+    <message>
+        <location filename="mainwindow.ui" line="106"/>
+        <source>Разное</source>
+        <translation>Other</translation>
+    </message>
+    <message>
+        <location filename="mainwindow.ui" line="117"/>
+        <location filename="mainwindow.cpp" line="207"/>
+        <source>Открыть файл только для чтения</source>
+        <translation>Open file readonly</translation>
+    </message>
+    <message>
+        <location filename="mainwindow.ui" line="122"/>
+        <location filename="mainwindow.cpp" line="189"/>
+        <source>Открыть файл</source>
+        <translation>Open file</translation>
+    </message>
+    <message>
+        <location filename="mainwindow.ui" line="127"/>
+        <source>Новый файл</source>
+        <translation>New file</translation>
+    </message>
+    <message>
+        <location filename="mainwindow.ui" line="132"/>
+        <source>Сохранить файл в txt</source>
+        <translation>Save file to txt</translation>
+    </message>
+    <message>
+        <location filename="mainwindow.ui" line="137"/>
+        <source>Сохранить файл в bin</source>
+        <translation>Save file to bin</translation>
+    </message>
+    <message>
+        <location filename="mainwindow.ui" line="142"/>
+        <source>Справка</source>
+        <translation>About</translation>
+    </message>
+    <message>
+        <location filename="mainwindow.ui" line="147"/>
+        <source>Translate</source>
+        <translation>Перевод</translation>
+    </message>
+    <message>
+        <location filename="mainwindow.ui" line="152"/>
+        <source>Выход</source>
+        <translation>Quit</translation>
+    </message>
+    <message>
+        <location filename="mainwindow.ui" line="157"/>
+        <source>Настройки</source>
+        <translation>Settings</translation>
+    </message>
+    <message>
+        <location filename="mainwindow.ui" line="162"/>
+        <source>Печать файла</source>
+        <translation>Print file</translation>
+    </message>
+    <message>
+        <location filename="mainwindow.cpp" line="21"/>
+        <source>Ширина таблицы</source>
+        <translation>Width table</translation>
+    </message>
+    <message>
+        <location filename="mainwindow.cpp" line="22"/>
+        <source>Высота таблицы</source>
+        <translation>Height table</translation>
+    </message>
+    <message>
+        <location filename="mainwindow.cpp" line="38"/>
+        <source>Форматирование текста</source>
+        <translation>Format text</translation>
+    </message>
+    <message>
+        <location filename="mainwindow.cpp" line="41"/>
+        <source>Сохранить формат выделеного текста</source>
+        <translation>Save format text from selected text</translation>
+    </message>
+    <message>
+        <location filename="mainwindow.cpp" line="42"/>
+        <source>Применить сохраненный формат на выделенногом тексте</source>
+        <translation>Apply text format to selected text</translation>
+    </message>
+    <message>
+        <location filename="mainwindow.cpp" line="48"/>
+        <source>Выравнивание текста</source>
+        <translation>Align text</translation>
+    </message>
+    <message>
+        <location filename="mainwindow.cpp" line="51"/>
+        <source>По левому краю</source>
+        <translation>Align left</translation>
+    </message>
+    <message>
+        <location filename="mainwindow.cpp" line="52"/>
+        <source>По центру</source>
+        <translation>Align center</translation>
+    </message>
+    <message>
+        <location filename="mainwindow.cpp" line="53"/>
+        <source>По правому краю</source>
+        <translation>Align right</translation>
+    </message>
+    <message>
+        <location filename="mainwindow.cpp" line="61"/>
+        <source>Установить шрифт</source>
+        <translation>Apply font</translation>
+    </message>
+    <message>
+        <location filename="mainwindow.cpp" line="227"/>
+        <source>Сохранить файл в .txt формате</source>
+        <translation>Save file to txt</translation>
+    </message>
+    <message>
+        <location filename="mainwindow.cpp" line="245"/>
+        <source>Автор: </source>
+        <translation>Author: </translation>
+    </message>
+    <message>
+        <location filename="mainwindow.cpp" line="247"/>
+        <source>Сохранить файл в .bin формате</source>
+        <translation>Save file to bin</translation>
+    </message>
+    <message>
+        <location filename="mainwindow.cpp" line="263"/>
+        <source>:/information/assets/text_editor_info/text_editor_info_EN.txt</source>
+        <translation>:/information/assets/text_editor_info/text_editor_info_Ru.txt</translation>
+    </message>
+    <message>
+        <location filename="mainwindow.cpp" line="268"/>
+        <source>Информация о текстовом редакторе</source>
+        <translation>Info</translation>
+    </message>
+    <message>
+        <location filename="mainwindow.cpp" line="303"/>
+        <source>Печать</source>
+        <translation>Print</translation>
+    </message>
+</context>
+<context>
+    <name>settings_window</name>
+    <message>
+        <location filename="settings.ui" line="14"/>
+        <source>Наcтройки</source>
+        <translation>Settings</translation>
+    </message>
+    <message>
+        <location filename="settings.ui" line="40"/>
+        <source>Новый файл</source>
+        <translation>New file</translation>
+    </message>
+    <message>
+        <location filename="settings.ui" line="78"/>
+        <location filename="settings.ui" line="133"/>
+        <location filename="settings.ui" line="188"/>
+        <location filename="settings.ui" line="243"/>
+        <location filename="settings.ui" line="298"/>
+        <location filename="settings.ui" line="319"/>
+        <location filename="settings.ui" line="374"/>
+        <location filename="settings.ui" line="429"/>
+        <location filename="settings.ui" line="484"/>
+        <location filename="settings.ui" line="539"/>
+        <source>Сброс</source>
+        <translation>Reset</translation>
+    </message>
+    <message>
+        <location filename="settings.ui" line="101"/>
+        <source>Открыть файл</source>
+        <translation>Open file</translation>
+    </message>
+    <message>
+        <location filename="settings.ui" line="156"/>
+        <source>Открыть файл только для чтения</source>
+        <translation>Open file readOnly</translation>
+    </message>
+    <message>
+        <location filename="settings.ui" line="211"/>
+        <source>Сохранить файл в txt</source>
+        <translation>Save file to txt</translation>
+    </message>
+    <message>
+        <location filename="settings.ui" line="266"/>
+        <source>Сохранить файл в bin</source>
+        <translation>Save file to bin</translation>
+    </message>
+    <message>
+        <location filename="settings.ui" line="309"/>
+        <source>Печать файла</source>
+        <translation>Print file</translation>
+    </message>
+    <message>
+        <location filename="settings.ui" line="342"/>
+        <source>Выход</source>
+        <translation>Quit</translation>
+    </message>
+    <message>
+        <location filename="settings.ui" line="397"/>
+        <source>Перевод</source>
+        <translation>Translate</translation>
+    </message>
+    <message>
+        <location filename="settings.ui" line="452"/>
+        <source>Настройки</source>
+        <translation>Settings</translation>
+    </message>
+    <message>
+        <location filename="settings.ui" line="507"/>
+        <source>Справка</source>
+        <translation>About</translation>
+    </message>
+    <message>
+        <location filename="settings.ui" line="562"/>
+        <source>Визуальный стиль текстового редактора</source>
+        <translation>Visual style</translation>
+    </message>
+    <message>
+        <location filename="settings.ui" line="570"/>
+        <source>Светлый</source>
+        <translation>Light</translation>
+    </message>
+    <message>
+        <location filename="settings.ui" line="575"/>
+        <source>Темный</source>
+        <translation>Dark</translation>
+    </message>
+    <message>
+        <location filename="settings.ui" line="580"/>
+        <source>Желтый</source>
+        <translation>Yellow</translation>
+    </message>
+    <message>
+        <location filename="settings.ui" line="585"/>
+        <source>Синий</source>
+        <translation>Blue</translation>
+    </message>
+    <message>
+        <location filename="settings.ui" line="597"/>
+        <source>Применить</source>
+        <translation>Accept</translation>
+    </message>
+    <message>
+        <location filename="settings.ui" line="604"/>
+        <source>Отменить</source>
+        <translation>Cancel</translation>
+    </message>
+</context>
+</TS>
